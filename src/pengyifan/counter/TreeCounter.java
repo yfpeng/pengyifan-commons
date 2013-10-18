822aba654b766493c46046e36cd1ef386ebac219
Yifan Peng <ypeng@cis.udel.edu>
2013-10-17 23:47:55 -0400

    add comparator in TreeCounter

--- src/pengyifan/counter/TreeCounter.java
+++ src/pengyifan/counter/TreeCounter.java
@@ -1,5 +1,6 @@
 package pengyifan.counter;
 
+import java.util.Comparator;
 import java.util.TreeMap;
 
 public class TreeCounter<K> extends Counter<K> {
@@ -13,12 +14,30 @@ public class TreeCounter<K> extends Counter<K> {
   }
 
   /**
+   * Constructs a new (empty) Counter.
+   */
+  public TreeCounter(Comparator<? super K> comparator) {
+    map = new TreeMap<K, MutableInteger>(comparator);
+    totalCount = 0;
+  }
+
+  /**
    * Constructs a new Counter with the contents of the given Counter.
    * 
    * @param counter
    */
   public TreeCounter(Counter<K> counter) {
     this();
+    addAll(counter);
+  }
+
+  /**
+   * Constructs a new Counter with the contents of the given Counter.
+   * 
+   * @param counter
+   */
+  public TreeCounter(Counter<K> counter, Comparator<? super K> comparator) {
+    this(comparator);
     addAll(counter);
   }
 
