fd2dd2650f29c25b22fdeb7509d0a4b83a6983fe
Yifan Peng <yfpeng@google.com>
2014-06-27 11:37:28 -0700

    embrace maven

---
+++ src/test/java/com/pengyifan/commons/lang/string/PtbStringTest.java
@@ -0,0 +1,25 @@
+package com.pengyifan.commons.lang.string;
+
+import com.pengyifan.commons.collections.tree.TreeNode;
+import com.pengyifan.commons.lang.string.PtbString;
+
+/**
+ * @author Yifan Peng
+ * @version 09/18/2013
+ */
+@Deprecated
+public class PtbStringTest {
+
+  public static void main(String args[]) {
+    TreeNode a = new TreeNode("a");
+    TreeNode c = new TreeNode("c");
+    a.add(new TreeNode("b"));
+    a.add(c);
+    a.add(new TreeNode("d"));
+
+    c.add(new TreeNode("e"));
+    c.add(new TreeNode("f"));
+
+    System.out.println(PtbString.toString(a));
+  }
+}
