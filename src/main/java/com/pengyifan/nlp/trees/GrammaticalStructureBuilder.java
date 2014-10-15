1edb430d485ec47a945ca2eb9269f59982662f3a
Yifan Peng <yfpeng@udel.edu>
2014-10-14 20:39:17 -0400

    change gs builder

--- src/main/java/com/pengyifan/nlp/trees/GrammaticalStructureBuilder.java
+++ src/main/java/com/pengyifan/nlp/trees/GrammaticalStructureBuilder.java
@@ -1,8 +1,6 @@
 package com.pengyifan.nlp.trees;
 
-import org.apache.commons.lang3.Validate;
-
-import com.google.inject.Inject;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 import edu.stanford.nlp.trees.GrammaticalStructure;
 import edu.stanford.nlp.trees.GrammaticalStructureFactory;
@@ -12,14 +10,13 @@ public class GrammaticalStructureBuilder {
 
   private final GrammaticalStructureFactory grammaticalStructureFactory;
   
-  @Inject
   public GrammaticalStructureBuilder(
       GrammaticalStructureFactory grammaticalStructureFactory) {
     this.grammaticalStructureFactory = grammaticalStructureFactory;
   }
   
   public GrammaticalStructure build(Tree tree) {
-    Validate.notNull(tree, "Tree is null");
+    checkNotNull(tree, "Tree is null");
     return grammaticalStructureFactory
         .newGrammaticalStructure(tree);
   }
